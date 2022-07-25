1ef5170da89d8566a3addf1cd3cdbf4212595261
VinitSingh <vinitsingh227@outlook.com>
2022-07-25 19:49:54 +0530

    Add project files.

---
+++ QueueDemo/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="QueueDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table style="border: 1px solid black; text-align:center; font-family:Arial">
+            <tr>
+                <td>Counter 1</td>
+                <td></td>
+                <td>Counter 2</td>
+                <td></td>
+                <td>Counter 3</td>
+            </tr>
+            <tr>
+                <td><asp:TextBox ID="txtCounter1" runat="server" BackColor="#3333FF" ForeColor="#CCCCFF" Width="150px"></asp:TextBox></td>
+                <td></td>
+                <td><asp:TextBox ID="txtCounter2" runat="server" BackColor="#3333FF" ForeColor="#9999FF" Width="150px"></asp:TextBox></td>
+                <td></td>
+                <td><asp:TextBox ID="txtCounter3" runat="server" BackColor="#3333FF" ForeColor="#CCCCFF" Width="150px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Button runat="server" ID="btnCounter1" Text="Next" Width="150px" OnClick="btnCounter1_Click"></asp:Button></td>
+                <td></td>
+                <td><asp:Button runat="server" ID="btnCounter2" Text="Next" Width="150px" OnClick="btnCounter2_Click"></asp:Button></td>
+                <td></td>
+                <td><asp:Button runat="server" ID="btnCounter3" Text="Next" Width="150px" OnClick="btnCounter3_Click"></asp:Button></td>
+            </tr>
+           
+            <tr>
+                <td colspan="5">
+                <asp:TextBox ID="txtDisplay" runat="server" BackColor="#3333FF" ForeColor="#CCCCFF" Width="500px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <asp:ListBox ID="listTokens" Font-Size="Large" runat="server"></asp:ListBox>
+                </td>
+            </tr>
+
+             <tr>
+                <td colspan="5">
+                    <asp:Button runat="server" ID="btnPrintTokens" Text="Print Tokens" Width="150px" OnClick="btnPrintTokens_Click"></asp:Button>
+                </td>
+            </tr>
+
+             <tr>
+                <td colspan="5">
+                    <asp:Label ID="lblStatus" runat="server" Font-Size="Large"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ QueueDemo/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueueDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["TokenQueue"] == null)
+            {
+                Queue<int> queueTokens = new Queue<int>();
+                Session["TokenQueue"] = queueTokens;
+            }
+
+        }
+
+        protected void btnPrintTokens_Click(object sender, EventArgs e)
+        {
+            Queue<int> tokenQueue = (Queue<int>)Session["TokenQueue"];
+            lblStatus.Text = "There are " + tokenQueue.Count.ToString() + "customers  before you in the queue";
+
+            if (Session["LastTokenNumberIssued"] == null)
+            {
+                Session["LastTokenNumberIssued"] = 0;
+            }
+
+            int nextTokenNumberToBeIssued = (int)Session["LastTokenNumberIssued"] + 1;
+            Session["LastTokenNumberIssued"] = nextTokenNumberToBeIssued;
+
+            tokenQueue.Enqueue(nextTokenNumberToBeIssued);
+            AddTokensToListBox(tokenQueue);
+
+        }
+
+        private void AddTokensToListBox(Queue<int> tokenQueue)
+        {
+            listTokens.Items.Clear();
+            foreach (int token in tokenQueue)
+            {
+                listTokens.Items.Add(token.ToString());
+            }
+        }
+
+        protected void btnCounter1_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomers(txtCounter1, 1);
+        }
+
+        protected void btnCounter2_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomers(txtCounter2, 2);
+        }
+
+        protected void btnCounter3_Click(object sender, EventArgs e)
+        {
+            ServeNextCustomers(txtCounter3, 3);
+        }
+
+
+        private void ServeNextCustomers(TextBox textBox, int counterNumbers)
+        {
+            Queue<int> tokenQueue = (Queue<int>)Session["TokenQueue"];
+            if (tokenQueue.Count <= 0)
+            {
+                textBox.Text = "No customers in the Queue";
+            }
+            else
+            {
+                int tokenNumberToBeServed = tokenQueue.Dequeue();
+                textBox.Text = tokenNumberToBeServed.ToString();
+                txtDisplay.Text = " Token Number : " + tokenNumberToBeServed.ToString() + " Please go to counter " + counterNumbers;
+            }
+
+            AddTokensToListBox(tokenQueue);
+        }
+    }
+}
